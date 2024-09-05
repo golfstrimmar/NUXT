8eb49c754b19ecc2025020cd7a56d6530f18e11c
golfstrimmar <64115098+golfstrimmar@users.noreply.github.com>
2024-09-05 15:56:33 +0200

    einz

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -7,6 +7,8 @@ header
           NuxtLink(to='/') Home
         li
           NuxtLink(to='/about') About
+        li
+          NuxtLink(to='/users') Users
 
 
 </template>
---
+++ pages/users/[id].vue
@@ -0,0 +1,7 @@
+<template lang='pug'>
+h1 user : {{ id }}
+</template>
+<script setup>
+const { id } = useRoute().params
+</script>
+<style lang='scss' scoped></style>
